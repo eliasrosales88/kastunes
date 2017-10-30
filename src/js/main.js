b692f9f9bff738a4983d580633318a4b78341e8a
“Elias <eliasrosales88@gmail.com>
2017-10-31 00:15:25 +0100

    Hemos creado el archivo db.json el cual simula una informacion como un objeto json para usarlo en el proyecto (consumir recursos como si se tratara de una api) Igualmente hemos modificado el archivo package.json ya que hemos sustituido "test" por "server y hemos habilitado el puerto 3100". En el archivo gulpfile hemos cambiado el servidor para desarrollo usando "proxy" con el puerto 3100 que es un puerto nuevo. Hemos borrado el contenido html de la etiqueta ideal del archivo songs-list.html para dejar unicamente las etiquetas con sus clases. Toda la manipulacion del DOM se realiza con jQuery y asignando el contenido con referencia a las clases de los estados "empty", "loading", "ideal" y "error"

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
-let $ = require("jquery");
+window.$ = window.jQuery = require("jquery");
+
+//Cargar la lista de canciones con AJAX
+
+$.ajax({
+    url:"/songs/",
+    success: songs =>{
+        // Comprobamos si hay canciones
+
+        if (songs.length == 0){
+
+            //Mostramos el estado vacio
+            $(".songs-list").removeClass("loading").addClass("empty");
+        }else{
+
+            // Componemos el html con todas las canciones
+            let html = "";
+            for (let song of songs){
+            html += `<article class="song">
+                <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
+                <div class="artist">${song.artist}</div>
+                <div class="title">${song.title}</div>
+            </article>`;
+            }
+
+            // Metemos el html en el div que contiene las canciones
+            $(".songs-list .ui-status.ideal").html(html);
+
+            //Quitamos el mensaje de cargando y mostrando el listado de canciones
+
+            $(".songs-list").removeClass("loading").addClass("ideal");
+        }   
+    }, 
+    error: error =>{
+
+        //Mostrar el estado de error
+        $(".songs-list").removeClass("loading").addClass("error");
+        console.error("Error al cargar las canciones", error);
+    }
+
+})
